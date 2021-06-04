77bf915aafd41ff9928cafe79761c8213ab892ef
Mohammad Taher <zohebtaher@gmail.com>
2021-06-03 23:04:22 -0400

    some css added

--- dist/bundle.js/bundle.js
+++ dist/bundle.js/bundle.js
@@ -13,7 +13,57 @@ var __webpack_exports__ = {};
 let isDrawing = false;
 let x = 0;
 let y = 0;
-// let pixels =
+let pixels = null;
+let alphabetpixels = null;
+let alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let letter = null;
+
+function toolSetup() {
+  drawletter();
+  pixels = context.getImageData(0, 0, myPics.width, myPics.height);
+  //   alphabetpixels = totalpixels(158, 101, 101);
+}
+
+function displayError() {
+  isDrawing = false;
+  alert("Oh Oh Try Again !");
+  context.clearRect(0, 0, myPics.width, myPics.height);
+  drawletter(letter);
+}
+
+function getpixelcolor(x, y) {
+  let index = y * (pixels.width * 4) + x * 4;
+  return {
+    r: pixels.data[index],
+    g: pixels.data[index + 1],
+    b: pixels.data[index + 2],
+    a: pixels.data[index + 3],
+  };
+}
+
+function totalpixels(r, g, b) {
+  const pixelData = context.getImageData(0, 0, 640, 680);
+  const all = pixelData.data.length;
+  let amount = 0;
+  for (i = 0; i < all; i += 4) {
+    if (
+      pixelData.data[i] === r &&
+      pixelData.data[i + 1] === g &&
+      pixelData.data[i + 2] === b
+    ) {
+      amount++;
+    }
+  }
+  return amount;
+}
+
+function calcThreshold() {
+  if (totalpixels(173, 255, 47) / totalpixels(255, 250, 250) > 0.89) {
+    alert("Yay Good Job !!");
+    context.clearRect(0, 0, myPics.width, myPics.height);
+    toolSetup();
+  }
+}
 
 const myPics = document.getElementById("tool-canvas");
 const context = myPics.getContext("2d");
@@ -55,6 +105,7 @@ window.addEventListener("mouseup", (e) => {
     x = 0;
     y = 0;
     isDrawing = false;
+    calcThreshold();
   }
 });
 
@@ -64,33 +115,43 @@ window.addEventListener("touchend", (e) => {
     x = 0;
     y = 0;
     isDrawing = false;
+    calcThreshold();
   }
 });
 
 function drawLine(context, x1, y1, x2, y2) {
-  context.beginPath();
-  context.strokeStyle = "black";
-  context.lineWidth = 20;
-  context.lineCap = "round";
-  context.moveTo(x1, y1);
-  context.lineTo(x2, y2);
-  context.stroke();
-  context.closePath();
+  let color = getpixelcolor(x2, y2);
+  if (color.a === 0) {
+    displayError();
+  } else {
+    context.beginPath();
+    context.strokeStyle = "rgb(173,255,47)";
+    context.lineWidth = 25;
+    context.lineCap = "round";
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+  }
 }
 
-function drawletter(letter) {
-  const centerx = (myPics.width - context.measureText(letter).width) / 3;
+function drawletter(empty) {
+  let chars = alphabets.split("");
+  letter = empty || chars[parseInt(Math.random() * chars.length, 10)];
+  context.font = "430px Arial";
+  context.fillStyle = "rgb(255,250,250)";
+  context.textBaseline = "middle";
+  const centerx = (myPics.width - context.measureText(letter).width) / 2;
   const centery = myPics.height / 2;
 
-  context.font = "300px Arial";
-  context.fillStyle = "rgb(255,255,0)";
-  context.textBaseline = "middle";
   context.fillText(letter, centerx, centery);
 }
 
-drawletter("A");
+toolSetup();
 
-// console.log("Webpack is working!");
+console.log(totalpixels(255, 255, 0));
+console.log(totalpixels(165, 42, 42));
+console.log(totalpixels(158, 101, 101));
 
 /******/ })()
 ;
